1e8b144f5bb674c48fe96cc6305503f5b09261dc
kennyb66 <kenneth.e.bartel-1@ou.edu>
2026-02-23 15:44:22 -0600

    Adding Inputfield.tsx for text entry box, styling for InputField.tsx and App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import InputField from './components/InputField';
+import { Todo } from './model';
 
-let name: string;
-let age: number | string;
+// let name: string;
+// let age: number | string;
 
-age = "Thirty";
-age = 30;
+// age = "Thirty";
+// age = 30;
 
-let isStudent: boolean;
-let hobbies: string[];
-let role: [number, string];
+// let isStudent: boolean;
+// let hobbies: string[];
+// let role: [number, string];
 
-let printName: (name:string) => never;
+// let printName: (name:string) => never;
 
-printName("Kenny");
+// // type Person = {
+// //   name: string;
+// //   age?: number;
+// // }
 
-// type Person = {
+// // let person: Person = {
+// //   name: "Kenny",
+// // };
+
+// // let lotsOfPeople: Person[] = [
+// //   {
+// //     name: "Kenny",
+// //     age: 30
+// //   },
+// //   {
+// //     name: "Alice",
+// //     age: 25
+// //   }
+// // ];
+
+// let personName: unknown;
+
+
+// interface Person extends X{
 //   name: string;
 //   age?: number;
-// }
-
-// let person: Person = {
-//   name: "Kenny",
 // };
 
-// let lotsOfPeople: Person[] = [
-//   {
-//     name: "Kenny",
-//     age: 30
-//   },
-//   {
-//     name: "Alice",
-//     age: 25
-//   }
-// ];
+// interface Guy extends Person {
+//   profession: string;
+// }
 
-let personName: unknown;
+// // Example of type extension
+// type X = {
+//   a: string;
+//   b: number;
+// };
 
-function App() {
+// type Y = X &{
+//   c: string;
+//   d: number;
+// };
+
+// let y: Y = {
+//   a: "Hello",
+//   b: 42,
+//   c: "Hello",
+//   d: 42
+// }
+
+const App : React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos,{id: Date.now(), todo, isDone: false}]);
+      setTodo("");
+    }
+  };
+
+  console.log(todos);
   return (
     <div className="App">
-      Hello World
+      <span className="heading">Taskify</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd = {handleAdd} />
     </div>
   );
 }
